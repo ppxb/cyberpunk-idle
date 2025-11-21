1cd9c7719df09034a751be290fbdf2cd82875b36
ppxb <317842449@qq.com>
2025-11-21 14:16:35 +0800

    chore: update project

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { createApp } from 'vue'
 
-import { setupStore } from '@/store'
-
 import App from './App.vue'
 import 'uno.css'
 
 async function bootstrap() {
   const app = createApp(App)
-
-  setupStore(app)
 
   app.mount('#app')
 }
